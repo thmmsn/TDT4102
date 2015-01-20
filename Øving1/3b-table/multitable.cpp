ed15eca26ea190cd3173f4cca7847d9b241bbe59
Fredrik Thommesen <thommesen@me.com>
2015-01-20 15:46:23 +0100

    second updt

--- Øving1/3b-table/multitable.cpp
+++ Øving1/3b-table/multitable.cpp
@@ -1,23 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int inputSize(){
-	int x;
-	cin >> x;
-	return x;
+int inputValues(){
+	int value;
+	cout << "skriv inn" << endl;
+	cin >> value;
+	return value;
 }
 
-int generateArray(bredde, hoyde){
-	
+
+int multiplyTable(int x, int y){
+	for(int i=1; i<=x; i++){
+		cout << i << " |";
+		for(int j=1; j<=y; j++){
+			cout << i*j << '\t';
+		}
+		cout << endl;
+	}
+	return 0;
 }
+
 
 int main(){
-	int bredde;
-	int hoyde;
-	cout << "Skriv inn bredde" << endl;
-	bredde = inputSize();
-	cout << "Skriv inn høyde" << endl;
-	hoyde = inputSize();
-
+	int h = inputValues();
+	int b = inputValues();
+	cout << "En multiplikasjonstabell (X * Y)" << h << "*" << b << endl;
+	multiplyTable(h,b);
 
 }
