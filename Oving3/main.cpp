f50d0567aab7d2aea37b639197d763348fba99b5
Fredrik Thommesen <thommesen@me.com>
2015-02-03 11:20:10 +0100

    Øving 3

---
+++ Oving3/main.cpp
@@ -0,0 +1,10 @@
+//
+//  main.cpp
+//  Øving:   3
+//  Uke  :   6
+//  Created by fredrik thommesen on 03/02/15.
+//  Copyright (c) 2015 fredrik. All rights reserved.
+//
+
+#include <iostream>
+#include <<#header#>>
